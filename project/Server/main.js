f78914786c3f6543ec4b9df67f38c53361c918fb
Vladimir Galushko <humanamburu@ojooo.com>
2016-04-02 15:10:59 +0300

    ES6 syntax SERVER changes

--- project/Server/main.js
+++ project/Server/main.js
@@ -1,59 +1,58 @@
-var express = require('express'),
-    app = express(),
-    mongoose = require('mongoose'),
-    config = require('./config.json'),
-    bodyParser = require('body-parser'),
-    cookieParser = require('cookie-parser'),
-    session = require('express-session'),
-    MongoStore = require('connect-mongo')(session),
-    controllers = require('./controllers/main.js'),
-    fs = require('fs'),
-    morgan = require('morgan'),
-    FileStreamRotator = require('file-stream-rotator'),
-    uuid = require('node-uuid'),
-    logger = require('./logger');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const config = require('./config.json');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const controllers = require('./controllers/main.js');
+const fs = require('fs');
+const morgan = require('morgan');
+const FileStreamRotator = require('file-stream-rotator');
+const logger = require('./logger');
+const logDirectory = `${__dirname}/log`;
 
 
 logger('');
 logger('Running server...');
-var server = app.listen(8080);
-mongoose.connect(config.mongo, function(error) {
-    if (error) throw error;
+const server = app.listen(8080);
+mongoose.connect(config.mongo, (error) => {
+  if (error) throw error;
 });
 
 logger('Server run at localhost:8080');
 
-//MORGAN SETUP
-var logDirectory = __dirname + '/log';
-fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
-var accessLogStream = FileStreamRotator.getStream({
-    date_format: 'YYYYMMDD',
-    filename: logDirectory + '/access-%DATE%.log',
-    frequency: 'daily',
-    verbose: false
+//  MORGAN SETUP
+const directory = fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+const accessLogStream = FileStreamRotator.getStream({
+  date_format: 'YYYYMMDD',
+  filename: `${logDirectory}/access-%DATE%.log`,
+  frequency: 'daily',
+  verbose: false
 });
 app.use(morgan(':method :url :response-time', {
-    stream: accessLogStream
+  stream: accessLogStream
 }));
 
 
-//MODULES FOR EXPRESS
+//  MODULES FOR EXPRESS
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 app.use(bodyParser.json());
 
 logger('Parsers initial complete');
 app.use(session({
-    resave: false,
-    secret: 'wunderlist',
-    saveUninitialized: false,
-    maxAge: new Date(Date.now() + 3600000),
-    store: new MongoStore({
-        mongooseConnection: mongoose.connection,
-        touchAfter: 24 * 3600
-    })
+  resave: false,
+  secret: 'wunderlist',
+  saveUninitialized: false,
+  maxAge: new Date(Date.now() + 3600000),
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    touchAfter: 24 * 3600
+  })
 }));
 
 logger('Passport initialize');
@@ -65,7 +64,7 @@ logger('Wait for requests...');
 logger('');
 
 
-//ENTRY POINTS
+//  ENTRY POINTS
 app.use('/', express.static('./project/Client/'));
 
 app.post('/register', controllers.registration);
