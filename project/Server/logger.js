755c6e1d1e1e9da970aa1a8b3d7488d6b115b611
Vladimir Galushko <humanamburu@ojooo.com>
2016-04-02 15:23:30 +0300

    ES6 syntax SERVER logger and User schema changes

--- project/Server/logger.js
+++ project/Server/logger.js
@@ -1,11 +1,5 @@
-var friendlyTime = function() {
-    var now = new Date();
-    var friendlyDate = now.getHours() + ':' + now.getMinutes();
-    return friendlyDate;
-};
-
-var logger = function(log) {
-    console.log(' * ' + log);
+const logger = function logger(log) {
+  console.log(' * ' + log);
 };
 
 module.exports = logger;
--- project/Server/models/User.js
+++ project/Server/models/User.js
@@ -1,37 +1,37 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-var User = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        validate: {
-            validator: function(v) {
-                return /^[a-zA-Zа-яА-Я\s]+$/.test(v);
-            },
-            message: '{VALUE} is not a valid name!'
-        }
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-        validate: {
-            validator: function(v) {
-                return /^(\w|_|\.)+@\w+\.[a-z]+$/g.test(v);
-            },
-            message: '{VALUE} is not a valid email!'
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        validate: {
-            validator: function (v) {
-                /^(\w|[а-яА-Я_]){3,64}$/g.test(v);
-            },
-            message: '{VALUE} is not a valid password'
-        }
+const User = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function validate(v) {
+        return /^[a-zA-Zа-яА-Я\s]+$/.test(v);
+      },
+      message: '{VALUE} is not a valid name!'
     }
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: function validate(v) {
+        return /^(\w|_|\.)+@\w+\.[a-z]+$/g.test(v);
+      },
+      message: '{VALUE} is not a valid email!'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function validate(v) {
+        /^(\w|[а-яА-Я_]){3,64}$/g.test(v);
+      },
+      message: '{VALUE} is not a valid password'
+    }
+  }
 });
 
-module.exports = mongoose.model("User", User);
+module.exports = mongoose.model('User', User);
